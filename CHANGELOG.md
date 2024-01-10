e824a6719863e010f640678228873d014c45b2f8
suyu <13936562@qq.com>
2024-01-10 23:45:04 +0800

    release v0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # ChangeLog
 
+## 0.0.3
+
+- fix bug: everytime run run.sh, a new cronjob will be added 
+
 ## 0.0.2
 
 - add new feature: exports for google photos
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ CRON=${CRON:-"0 3 * * *"}
 
 touch /root/cron.out
 
-crontab -l | { cat; echo "${CRON} /usr/local/bin/python /app/main.py >> /root/cron.out 2>&1"; } | crontab -
+echo "${CRON} /usr/local/bin/python /app/main.py >> /root/cron.out 2>&1" | crontab -
 
 cron && tail -f /root/cron.out
